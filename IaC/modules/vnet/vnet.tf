4ffcd668cdcb04516fa056cf3e8d81b858b2a121
Fernando Asulay <fer.guitar.ln@gmail.com>
2021-12-04 19:38:37 +0100

    Added Network security groups and update readme

--- IaC/modules/vnet/vnet.tf
+++ IaC/modules/vnet/vnet.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
-  tags = var.tags
+  tags                = var.tags
 }
 
 resource "azurerm_subnet" "agw_subnet" {
@@ -14,24 +14,41 @@ resource "azurerm_subnet" "agw_subnet" {
   service_endpoints    = ["Microsoft.Web"]
 }
 
-# # SecurityGroup
-# resource "azurerm_network_security_group" "vnet_agw_sg" {
-#   name                = format(var.name_template, "asg-${var.solution}")
-#   location            = var.location
-#   resource_group_name = var.resource_group_name
+# SecurityGroup
+resource "azurerm_network_security_group" "vnet_agw_sg" {
+  name                = format(var.name_template, "agw-nsg")
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
-#   # App Gateway
-#   security_rule {
-#     name                       = "agw"
-#     priority                   = 100
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_ranges         = ["80", "443"]
-#     destination_port_range     = "*"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
+  # App Gateway
+  security_rule {
+    name                       = "agw"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "65200-65535"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-#   tags = var.tags
-# }
+  security_rule {
+    name                       = "Port_80"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg-agw" {
+  subnet_id                 = azurerm_subnet.agw_subnet.id
+  network_security_group_id = azurerm_network_security_group.vnet_agw_sg.id
+}
--- README.md
+++ README.md
@@ -6,8 +6,11 @@ This is a Terraform proof of concept on how to provision infrastructure to azure
 This repo is conform by three folders: 
 1. IaC (Infrastructure as code): where all the terraform code is in.
 2. todoservice an express js example backend application to test our infraestructure
-3. todoui an angular frontend application to test our infrastructure, using nginx to serve the site and proxy any /api request to the backend.
+3. todoui an angular frontend application to test our infrastructure.
 
-Our terraform code provisions a resource group and two app services configured to be deployed as containers, picking the image from dockerhub. I'm are also creating a custom domain for the frontend app using an already existing DNS Zone outside the resource group.
+Our terraform code provisions a resource group with:
+- Two app services configured to be deployed as containers, picking the image from dockerhub. I'm are also creating a custom domain for the frontend app using an already existing DNS Zone outside the resource group.
+- A vnet to hold all our resoureces as private.
+- An App Gateway as a main entrypoint to our network to access the app services.
 
 - NOTE: DNS Zone provisioning was manual due to the need of getting the DNS server urls posterior to its creation to be registered into the admin's portal of the domain's provider.
